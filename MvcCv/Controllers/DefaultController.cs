edf3a0b3a25930b7c58ccb9b7bfa1a680e89cc8f
Sertaç Ersan <sertacersan@hotmail.com>
2025-04-16 17:13:12 +0300

    #yeni

--- MvcCv/Controllers/DefaultController.cs
+++ MvcCv/Controllers/DefaultController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcCv.Models.Entity;
 namespace MvcCv.Controllers
 {
     public class DefaultController : Controller
     {
+        DbCvEntities db = new DbCvEntities();
         // GET: Default
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
         }
     }
 }
