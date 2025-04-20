5b6ef406129f643befa3a61bc6643b65d4ea4b1c
Sertaç Ersan <sertacersan@hotmail.com>
2025-04-21 02:11:29 +0300

    yeni

--- MvcCv/Controllers/DefaultController.cs
+++ MvcCv/Controllers/DefaultController.cs
@@ -15,5 +15,10 @@ namespace MvcCv.Controllers
             var degerler = db.TblHakkimda.ToList();
             return View(degerler);
         }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
     }
 }
